31630fa4f62cad101229e0810f2f523157f12de6
Tran Le Quang Trieu <trieutran025@gmail.com>
2024-05-20 21:01:55 +0700

    trieuTLQ

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import ItemList from './components/ItemList';
-import ItemDetail from './components/ItemDetail';
-import ItemCreate from './components/ItemCreate';
-import Cart from './components/Cart'; // Import Cart component
-
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout"
+import Home from "./page/Home"
+import Cart from "./page/Cart"
 function App() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Home</Link>
-          <Link className="navbar-brand" to="/create">Add Item</Link>
-          <Link className="navbar-brand" to="/cart">Cart</Link>
-        </div>
-      </nav>
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<ItemList />} />
-          <Route path="/items/:id" element={<ItemDetail />} />
-          <Route path="/create" element={<ItemCreate />} />
-          <Route path="/cart" element={<Cart />} /> {/* Add Cart route */}
-        </Routes>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
